c3b8b43ceec4b76233f9cd2c97e53f9cedf60629
pradeep <pradeepsn606@gmail.com>
2022-10-10 18:17:07 +0530

    first commit

---
+++ ThreeLetterAcronym.java
@@ -0,0 +1,23 @@
+package code;
+
+import java.util.Scanner;
+
+public class ThreeLetterAcronym {
+    public static void main(String args[]) {
+        String str = "", ans = "";
+        String arr[] = new String[3];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a three letter Abbreviation");
+        str = scan.nextLine();
+        arr = str.split(" ");
+        if (arr.length != 3) {
+            System.out.println("Enter a valid Abbreviation" + arr.length);
+
+            return;
+        }
+        for (int i = 0; i < 3; i++) {
+            ans += arr[i].charAt(0);
+        }
+        System.out.println(ans.toUpperCase());
+    }
+}
